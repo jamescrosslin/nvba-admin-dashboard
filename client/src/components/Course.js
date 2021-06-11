ed2f697f4e924f701ff212c93e73f49883f4cedd
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-11 15:12:41 -0700

    added data fetching to Courses component

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -4,9 +4,9 @@ function Course(props) {
   const { title, description } = props;
   return (
     // TODO: add routing to component
-    <a class="course--module course--link" href="course-detail.html">
-      <h2 class="course--label">{title}</h2>
-      <h3 class="course--title">{description}</h3>
+    <a className="course--module course--link" href="course-detail.html">
+      <h2 className="course--label">{title}</h2>
+      <h3 className="course--title">{description}</h3>
     </a>
   );
 }
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import Course from './Course';
+import axios from 'axios';
+
+const url = process.env.API_URL || 'http://localhost:5000';
 
 function Courses(props) {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch courses from api
+    async function getCourses() {
+      setLoading(true);
+      const { data: courses } = await axios.get(`${url}/api/courses`);
+      console.log(courses);
+      setCourses(courses);
+      setLoading(false);
+    }
+    getCourses();
   }, []);
+
   return (
     <React.Fragment>
-      {courses.map((course) => {
-        <Course key={course.id} {...course} />;
-      })}
+      {!isLoading && courses.map((course) => <Course key={course.id} {...course} />)}
     </React.Fragment>
   );
 }
