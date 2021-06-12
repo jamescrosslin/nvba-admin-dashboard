869b2a7d3e6c6174438411974359f8009818962c
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-11 19:02:15 -0700

    added new course creation option to Courses

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Course from './Course';
 import axios from 'axios';
-
-const url = process.env.API_URL || 'http://localhost:5000';
+import { url } from '../utils';
 
 function Courses(props) {
   const [courses, setCourses] = useState(null);
@@ -13,7 +13,6 @@ function Courses(props) {
     async function getCourses() {
       setLoading(true);
       const { data: courses } = await axios.get(`${url}/api/courses`);
-      console.log(courses);
       setCourses(courses);
       setLoading(false);
     }
@@ -23,6 +22,21 @@ function Courses(props) {
   return (
     <React.Fragment>
       {!isLoading && courses.map((course) => <Course key={course.id} {...course} />)}
+      <Link to="/courses/create" className="course--module course--add--module">
+        <span class="course--add--title">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 13 13"
+            class="add"
+          >
+            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+          </svg>
+          New Course
+        </span>
+      </Link>
     </React.Fragment>
   );
 }
