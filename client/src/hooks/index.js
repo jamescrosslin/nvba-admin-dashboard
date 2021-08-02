208edb35d3ccab5ac1965f201aed2b44a9a0bf6a
James Crosslin <jamesmscrosslin@gmail.com>
2021-08-02 12:08:13 -0700

    fix memory leak

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { url } from '../config';
 import axios from 'axios';
 import { useUserContext } from '../context/UserContext';
@@ -6,34 +6,35 @@ import { useUserContext } from '../context/UserContext';
 export function useFetchData(initialValues) {
   const { user } = useUserContext();
   const [{ isLoading, data, error }, setState] = useState(initialValues);
+  const fetchData = useCallback(
+    async function ({ apiRoute, method, isAuthRequired, requestBody, transformFunction }) {
+      try {
+        setState({ isLoading: true });
 
-  async function fetchData({ apiRoute, method, isAuthRequired, requestBody, transformFunction }) {
-    try {
-      setState({ isLoading: true });
+        const requestOptions = {
+          method,
+          url: `${url}${apiRoute}`,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
 
-      const requestOptions = {
-        method,
-        url: `${url}${apiRoute}`,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
+        if (requestBody) requestOptions.data = requestBody;
+        if (isAuthRequired)
+          requestOptions.auth = { username: user.username, password: user.password };
 
-      if (requestBody) requestOptions.data = requestBody;
-      if (isAuthRequired)
-        requestOptions.auth = { username: user.username, password: user.password };
+        const { data } = await axios(requestOptions);
 
-      const { data } = await axios(requestOptions);
+        if (transformFunction) transformFunction(data);
 
-      if (transformFunction) transformFunction(data);
-
-      setState({ data });
-      setState({ isLoading: false });
-    } catch (err) {
-      console.dir(err);
-      setState({ error: err });
-    }
-  }
+        setState({ data, isLoading: false });
+      } catch (err) {
+        console.dir(err);
+        setState({ error: err });
+      }
+    },
+    [user.username, user.password],
+  );
 
   return { isLoading, data, error, fetchData };
 }
